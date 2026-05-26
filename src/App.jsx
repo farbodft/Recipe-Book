a85cc771dc61a42c99b2f35b64c6827aa22c205d
farbodft <farbod.ft2005@gmail.com>
2026-05-26 16:23:02 +0330

    Add Navbar to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-/* eslint-disable no-unused-vars */
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import FavoritesPage from "./pages/FavoritesPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-      </Routes>
-    </BrowserRouter>
+    <HashRouter>
+      <div className="min-h-screen bg-slate-900">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
