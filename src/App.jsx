33cdbf8a8fa75259bdda9718e9d0d5209aa550cc
Mahsa Hajirahimi <mahsahajirahimi2003@gmail.com>
2026-05-26 16:19:32 +0330

    Update App with main wrapper and 404 route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-/* eslint-disable no-unused-vars */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import FavoritesPage from "./pages/FavoritesPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-      </Routes>
+      <main className="bg-black min-h-screen text-white p-10">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="*" element={<HomePage />} /> 
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
